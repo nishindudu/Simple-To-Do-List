16ce97767354b83120db0b9b2a709a2eeec4f041
Nishin Santhosh <nishindudu@gmail.com>
2025-02-22 23:13:39 +0530

    mongodb

--- main.py
+++ main.py
@@ -1,11 +1,76 @@
 import json
 import configparser
 import os
+import hashlib
 from flask import Flask, render_template, redirect, url_for, request
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("KEY")
+
+uri = os.environ.get("MONGO")
+
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+class db():
+    def __init__(self, id):
+        self.id = id
+
+    def ping_db():
+        try:
+            client.admin.command('ping')
+            return True
+        except:
+            return False
+    
+    def save_user(self, password):
+        db = client['todo']
+        users = db['users']
+        password = crypto().encrypt_sha256(password)
+        if users.find_one({'id': self.id}):
+            raise Exception('User already exists')
+        else:
+            users.insert_one({'id': self.id, 'password': password})
+    
+    def check_user(self, password):
+        db = client['todo']
+        users = db['users']
+        user = users.find_one({'id': self.id})
+        password = crypto().encrypt_sha256(password)
+        if user and user['password'] == password:
+            return True
+        return False
+    
+    def get_list(self):
+        db = client['todo']
+        lists = db['lists']
+        list = lists.find_one({'id': self.id})
+        if list:
+            return list['items']
+        return []
+    
+    def add_to_list(self, items):
+        if self.get_list() == []:
+            db = client['todo']
+            lists = db['lists']
+            lists.insert_one({'id': self.id, 'items': items})
+        else:
+            db = client['todo']
+            lists = db['lists']
+            lists.update_one({'id': self.id}, {'$push': {'items': {'$each': items}}})
+
+class crypto():
+    def __init__(self):
+        pass
+    
+    def encrypt_sha256(self, data):
+        return hashlib.sha256(data.encode()).hexdigest()
+    
+# print(crypto().encrypt_sha256('password'))
+# os.abort()
+
 
 
 login_manager = LoginManager()
@@ -72,7 +137,7 @@ class storage():
                     return True
         return False
 
-# storage('admin').add_to_list(['Task 1', 'Task 2', 'Task 3'])
+# db('admin').add_to_list(['Task 1', 'Task 2', 'Task 3'])
 
 class User(UserMixin):
     def __init__(self, id):
@@ -92,7 +157,7 @@ def login():
         #     user = User(id=username)
         #     login_user(user)
         #     return redirect(url_for('index'))
-        if storage(username).check_user(password):
+        if db(username).check_user(password):
             user = User(id=username)
             login_user(user)
             return redirect(url_for('index'))
@@ -104,8 +169,11 @@ def create_user():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        user = storage(username)
-        user.save_user(password)
+        user = db(username)
+        try:
+            user.save_user(password)
+        except Exception as e:
+            print(e)
     return render_template('register.html')
 
 @app.route('/logout')
@@ -117,13 +185,13 @@ def logout():
 @app.route('/login', methods=['GET'])
 @login_required
 def index():
-    return render_template('index.html', tasks=storage(current_user.id).get_list())
+    return render_template('index.html', tasks=db(current_user.id).get_list())
 
 @app.route('/add_item', methods=['POST'])
 @login_required
 def add_task():
     task = request.form['task']
-    storage(current_user.id).add_to_list([task])
+    db(current_user.id).add_to_list([task])
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
